fe83d56ed43b7634386d61a46baca4d6a18914a4
sungkwang <sungkwang@wantedlab.com>
2022-12-01 14:17:32 +0900

    feat: prettier

--- apps/wanted/pages/index.tsx
+++ apps/wanted/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { sayHello } from "@wanted/lib";
-import { Message } from "@wanted/ui"
+import { Message } from "@wanted/ui";
 
 export default function Home() {
   return (
@@ -22,8 +22,7 @@ export default function Home() {
         <Message message="Hello From @wanted/ui"></Message>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -37,10 +36,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -52,9 +48,7 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
